65a9a22fa8386f3dca63d9d8cb31fc1b81b21c84
uno9 <ws060m@gmail.com>
2021-03-18 02:31:26 +0900

    [add]create todo for recoil

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TaskList } from './Task';
+
+import {
+    RecoilRoot,
+} from 'recoil';
+
+
+function App() {
+    return (
+        <RecoilRoot>
+            <TaskList/>
+        </RecoilRoot>
+    );
+}
+
+export default App;
